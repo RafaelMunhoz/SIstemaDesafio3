6446b89e9c66c584a19f0098d90d11e5cebeaddf
07739587117 <07739587117@192.168.56.1>
2019-08-22 20:25:29 -0400

    UP

---
+++ SistemaHospitalar/src/br/com/hosptitalif/controller/Administrador.java
@@ -0,0 +1,30 @@
+package br.com.hosptitalif.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Administrador {
+
+    @FXML
+    private TextField txtLogin;
+
+    @FXML
+    private Button btnSalvar;
+
+    @FXML
+    private TextField txtSenha;
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private TextField txtStatusUsuario;
+
+    @FXML
+    void CadastrarFuncionario(ActionEvent event) {
+
+    }
+
+}
---
+++ SistemaHospitalar/src/br/com/hosptitalif/controller/Atendente.java
@@ -0,0 +1,12 @@
+package br.com.hosptitalif.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Atendente {
+
+    @FXML
+    private Button btnCadastrarPaciente;
+
+}
+
