3797fa8b40506de3b7096997c85867c06dcc6c9b
07739587117 <07739587117@192.168.56.1>
2019-08-29 19:22:26 -0400

    UP Login JAVAFX

---
+++ SistemaHospitalar/src/br/com/hosptitalif/controller/Login.java
@@ -0,0 +1,24 @@
+package br.com.hosptitalif.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Login {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private TextField tstSenha;
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
