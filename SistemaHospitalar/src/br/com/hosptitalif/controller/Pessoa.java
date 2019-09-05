dfad637519afbd1c0cb3667b6015b1d3a86a7121
User <User@DESKTOP-PNISOCH>
2019-09-05 16:52:14 -0300

    up

--- SistemaHospitalar/src/br/com/hosptitalif/controller/Pessoa.java
+++ SistemaHospitalar/src/br/com/hosptitalif/controller/Pessoa.java
@@ -3,19 +3,9 @@ package br.com.hosptitalif.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Pessoa {
-
-    @FXML
-    private Button btnSalvar;
-
-    @FXML
-    private Label txtStatusDaPessoa;
-
-    @FXML
-    private Button btnVoltar;
 
     @FXML
     private TextField txtNome;
@@ -24,17 +14,26 @@ public class Pessoa {
     private TextField txtCPF;
 
     @FXML
+    private TextField txtIdade;
+
+    @FXML
     private TextField txtSexo;
 
     @FXML
-    private TextField txtIdade;
+    private TextField txtStatusDaPessoa;
 
     @FXML
     private TextField txtTipoSanguineo;
 
     @FXML
-    void salvar(ActionEvent event) {
+    private Button btnSalvar;
 
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    void salvar(ActionEvent event) {
+    
     }
 
     @FXML
